b6cd8e3d3f721343b14fd2805a1478ad4e30f7fc
Archit Singhal <161698181+evilcap123@users.noreply.github.com>
2025-10-02 10:24:04 +0530

    Update server.js

--- vikas-backend/server.js
+++ vikas-backend/server.js
@@ -153,9 +153,9 @@ app.get("/chat-gemini-stream", async (req, res) => {
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Catch-all route for SPA
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// app.get(/.*/, (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 
 // 🚀 Start Server
